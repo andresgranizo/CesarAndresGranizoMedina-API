ddd6ab31cee42a6e8d307ba1db10a0124a2106bc
Andres Granizo <agranizo@ampagency.com>
2024-08-16 02:00:44 -0500

    API-REST

---
+++ README.md
@@ -0,0 +1,25 @@
+# Api Rest Tests
+
+This repository contains API tests for the signup and login functionalities of the Demoblaze website.
+
+## Prerequisites
+
+1. Install [Node.js](https://nodejs.org/) and npm (Node Package Manager).
+
+## Setup
+
+1. Clone the repository:
+   ```bash
+   git clone
+
+2. Install Dependencies
+   * npm install
+   * npm install cypress --save-dev
+
+# Runing Tests
+
+Local Execution
+
+1. npx cypress open
+2. Start E2E testing in Chrome
+3. Run tests (api-restspec.cy.js)
---
+++ cypress/e2e/api-restspec.cy.js
@@ -0,0 +1,49 @@
+describe('Demoblaze API Tests', () => {
+    const baseUrl = 'https://api.demoblaze.com';
+    const newUser = {
+        username: 'testuser123',
+        password: 'testpassword'
+    };
+    const existingUser = {
+        username: 'existinguser',
+        password: 'existingpassword'
+    };
+
+    it('Create a new user', () => {
+        cy.request('POST', `${baseUrl}/signup`, newUser)
+            .then((response) => {
+                expect(response.status).to.eq(200);
+            });
+    });
+
+    it('Attempt to create an existing user', () => {
+        cy.request({
+            method: 'POST',
+            url: `${baseUrl}/signup`,
+            body: newUser,
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            console.log(response);
+        });
+    });
+
+
+    it('Login with correct credentials', () => {
+        cy.request('POST', `${baseUrl}/login`, newUser)
+            .then((response) => {
+                expect(response.status).to.eq(200);
+            });
+    });
+
+    it('Login with incorrect credentials', () => {
+        cy.request({
+            method: 'POST',
+            url: `${baseUrl}/login`,
+            body: { username: 'wronguser', password: 'wrongpassword' },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+        });
+    });
+});
